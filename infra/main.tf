c9358fdddf9ccf8547d93c9f33cee9b54b6702b8
ashknth <ashoknath2076@gmail.com>
2025-05-28 18:27:54 -0400

    with same bucket

--- infra/main.tf
+++ infra/main.tf
@@ -2,52 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# S3 Bucket for your static resume website
-resource "aws_s3_bucket" "resume_site" {
-  bucket = "autocloudmate"
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-
-  tags = {
-    Name        = "autocloudmate"
-    Environment = "ResumeSite"
-  }
+variable "bucket_name" {
+  default = "autocloudmate"
 }
 
-# Origin Access Identity (OAI) to restrict S3 access to CloudFront only
-resource "aws_cloudfront_origin_access_identity" "oai" {
-  comment = "OAI for autocloudmate"
+variable "distribution_id" {
+  default = "E37CM87UA02ZMU"
 }
 
-# Bucket Policy to allow read access to CloudFront via OAI
-resource "aws_s3_bucket_policy" "allow_cf_read" {
-  bucket = aws_s3_bucket.resume_site.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontServicePrincipal"
-        Effect    = "Allow"
-        Principal = {
-          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
-        }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.resume_site.arn}/*"
-      }
-    ]
-  })
-}
-
-# CloudFront Cache Invalidation (triggered on every change)
+# CloudFront cache invalidation on every Terraform apply
 resource "null_resource" "invalidate_cloudfront" {
   provisioner "local-exec" {
     command = <<EOT
       aws cloudfront create-invalidation \
-        --distribution-id E37CM87UA02ZMU \
+        --distribution-id ${var.distribution_id} \
         --paths "/*"
     EOT
   }
